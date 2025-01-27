7319542794d24dbcfd0c4f62e4b73ce1df3298da
Maksym Postument <777rip777@gmail.com>
2025-01-27 17:28:44 +0200

    Added first task

---
+++ delegates/delegates/NotificationService.cs
@@ -0,0 +1,29 @@
+﻿namespace delegates;
+
+public delegate void NotificationHandler(string message);
+
+public class NotificationService
+{
+    public NotificationHandler Handler = null;
+
+    public NotificationService(NotificationHandler handler)
+    {
+        Handler = handler;
+    }
+    
+    public NotificationService(List<NotificationHandler> handlers)
+    {
+        foreach (var action in handlers)
+        {
+            Handler += action;
+        }
+    }
+    
+    public NotificationService() {}
+    
+    public void Send(string message)
+    {
+        
+        Handler?.Invoke(message);
+    }
+}
--- delegates/delegates/Program.cs
+++ delegates/delegates/Program.cs
@@ -11,3 +11,37 @@ fixErrors.Case7();
 fixErrors.Case8();
 fixErrors.Case9();
 fixErrors.Case10();
+
+NotificationService notificationService;
+Console.WriteLine("Choose notification method: 1 for Console, 2 for File, 3 for Both");
+string choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        notificationService = new NotificationService(ConsoleNotification);
+        break;
+    case "2":
+        notificationService = new NotificationService(FileNotification);
+        break;
+    case "3":
+        notificationService = new NotificationService(new List<NotificationHandler>() {FileNotification, ConsoleNotification});
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        return;
+}
+
+Console.WriteLine("Enter the message to send:");
+string message = Console.ReadLine();
+
+notificationService.Send(message);
+void ConsoleNotification(string message)
+{
+    Console.WriteLine(message);
+}
+
+void FileNotification(string message)
+{
+    File.AppendAllText("notifications.txt", message);
+}
