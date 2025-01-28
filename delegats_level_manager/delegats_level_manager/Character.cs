32c9289f926aab483b55320a131977c7b4951605
Maksym Postument <777rip777@gmail.com>
2025-01-28 16:49:16 +0200

    Add XP system and level-up event to Character class
    - Implemented AddXP method to handle XP addition and level-up logic
    - Added OnLevelUp event to notify when a character levels up

---
+++ delegats_level_manager/delegats_level_manager/Character.cs
@@ -0,0 +1,21 @@
+﻿namespace delegats_level_manager;
+
+public class Character
+{
+    public int Level { get; set; }
+    public int CurrentXP { get; set; }
+    public int XPForNextLevel { get; set; }
+    public event Action<Character, int> OnLevelUp;
+
+    public void AddXP(int amount)
+    {
+        CurrentXP += amount;
+        if (CurrentXP >= XPForNextLevel)
+        {
+            Level++;
+            CurrentXP = 0;
+            XPForNextLevel = Level * 100;
+            OnLevelUp?.Invoke(this, Level);
+        }
+    }
+}
